27fc12269dd13be7a4de302432f5c8744d2becb4
charles <charles.angila@student.moringaschool.com>
2024-10-01 10:29:24 +0300

    "Added age column to User model and updated database file"

--- models.py
+++ models.py
@@ -6,4 +6,5 @@ class  User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
+    age = db.Column(db.String, unique=True)
     
