a09dbea229776b084a677a645b4569dc63740684
charles <charles.angila@student.moringaschool.com>
2024-10-01 10:02:22 +0300

    "Added configuration for Flask app and database connection"

--- app.py
+++ app.py
@@ -2,15 +2,24 @@ from flask import Flask
 from flask_migrate import Migrate
 from models import db
 
-
+# create flask application object
 app = Flask(__name__)
 
+#configure a database connection to the local file app.db
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdb.db'
+
+#disable modification tracking to use less memory
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.json.compact = False
 
+#create a migrate object  to manage schema modification
 
 migrate = Migrate(app, db)
+
+#initialize the flask application to use the database
 db.init_app(app)
 
 @app.route('/')
